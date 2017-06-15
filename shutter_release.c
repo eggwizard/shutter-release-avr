2773e54a660902b712e8da510b76fd74f64f4169
eggwizard-ubuntu-mmc <eggwizard@naver.com>
2017-06-15 16:50:57 +0900

    Modified  - Time lag mode for self timer was eliminated to reduce code size Added  - Shot counter and period are displayed in run mode

--- shutter_release.c
+++ shutter_release.c
@@ -25,21 +25,24 @@
 // unsigned char temp1 = 0;
 // int temp2 = 0;
 
+#define MIN_SHUTTER_100MS	1
+#define MIN_PERIOD_100MS	5
+#define MIN_PREAMBLE_100MS	3
 
 #define STATE_MAIN 0
 #define STATE_SHUTTER_SPEED 1
-#define STATE_TIME_LAG 2
-#define STATE_PERIOD 3
-#define STATE_NUM_SHOT 4
+#define STATE_PERIOD 2
+#define STATE_NUM_SHOT 3
+#define STATE_TIME_LAG 4
 #define STATE_RUN 5
 
 
-unsigned int var_set_lagtime[5] = {0, 2, 5, 10};
+// unsigned int var_set_lagtime[5] = {0, 2, 5, 10};
 unsigned int var_set_shutter[25] = {0,1,2,4,6,8, 10, 13,15, 20, 25, 30, 40, 60, 80, 100, 130, 150, 200, 250, 300, 400, 500, 600, 610};
-unsigned int var_set_period[16] = {0, 1, 2, 4, 5, 8, 10, 20, 30, 50, 60, 100, 120, 180, 200, 210};
+unsigned int var_set_period[17] = {0, 1, 2, 3, 4, 5, 8, 10, 20, 30, 50, 60, 100, 120, 180, 200, 210};
 unsigned int var_set_num[13] = {1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 18, 20, 20};
 
-char msg_lagtime[] 	= "  T_l:%4d sec  ";
+// char msg_lagtime[] 	= "  T_l:%4d sec  ";
 char msg_shutter[] 	= "  T_s:%4d sec  ";
 char msg_period[] 	= "  T_p:%4d sec  ";
 char msg_num[] 		= "  N :%5d times";
@@ -56,24 +59,24 @@ typedef struct {
 } state_var_t;
 
 
-state_var_t state_var_lagtime = {
-	var_set_lagtime, 0, 10, 4, 0, msg_lagtime, 0};
+// state_var_t state_var_lagtime = {
+// 	var_set_lagtime, 0, 10, 4, 0, msg_lagtime, 0};
 
 state_var_t state_var_shutter = {
 	var_set_shutter, 0, 1200, 24, 1, msg_shutter, 10};
 
 state_var_t state_var_period = {
-	var_set_period, 1, 3600, 15, 1, msg_period, 10};
+	var_set_period, 1, 3600, 16, 1, msg_period, 10};
 
 state_var_t state_var_num = {
-	var_set_num, 1, 5000, 12, 1, msg_num, 5};
+	var_set_num, 2, 5000, 12, 1, msg_num, 5};
 
 
 
 unsigned int state_cur = STATE_MAIN;
 unsigned int state_var_menu = STATE_SHUTTER_SPEED;
 
-#define state_var_menu_NUM 4
+#define state_var_menu_NUM 3
 
 void sys_init(void)
 {
@@ -88,7 +91,7 @@ void sys_init(void)
 
 void test_func (void){
 
-	char msg_temp[20]={0};
+	// char msg_temp[20]={0};
 
 	// state_var_menu += get_enc_delta();
 	// state_var_menu = ( state_var_menu +MENU_LV0_NUM -1 )%MENU_LV0_NUM + 1;
@@ -207,11 +210,9 @@ void state_period_job(void){
 		sprintf(msg_temp2, "  T_p: 0.5 sec  ");
 	} else {
 		sprintf(msg_temp2, state_var_period.msg_format , state_var_period.var_set[state_var_period.idx]);
-	}
-	
+	}	
 	
 	print_text(0, 1, msg_temp2, OLED_PRINT_BLINK);
-
 
 	if (btn_click_input) {
 		state_cur = STATE_MAIN;
@@ -219,62 +220,70 @@ void state_period_job(void){
 	}
 }
 
-void state_timelag_job(void){
+// void state_timelag_job(void){
 
-	char msg_temp2[20] = {0};
-	int enc_input, btn_click_input, btn_hold_input;
+// 	char msg_temp2[20] = {0};
+// 	int enc_input, btn_click_input, btn_hold_input;
 	
-	enc_input = get_enc_delta();
-	btn_click_input = get_status_btn_click();
-	btn_hold_input = get_status_btn_hold();
+// 	enc_input = get_enc_delta();
+// 	btn_click_input = get_status_btn_click();
+// 	btn_hold_input = get_status_btn_hold();
 
-	set_state_var(&state_var_lagtime, enc_input);
+// 	set_state_var(&state_var_lagtime, enc_input);
 	
-	sprintf(msg_temp2, state_var_lagtime.msg_format, state_var_lagtime.var_set[state_var_lagtime.idx]);
-	print_text(0, 1, msg_temp2, OLED_PRINT_BLINK);
+// 	sprintf(msg_temp2, state_var_lagtime.msg_format, state_var_lagtime.var_set[state_var_lagtime.idx]);
+// 	print_text(0, 1, msg_temp2, OLED_PRINT_BLINK);
 
 
-	if (btn_click_input) {
-		state_cur = STATE_MAIN;
-		clear_oled();
-	}
-}
+// 	if (btn_click_input) {
+// 		state_cur = STATE_MAIN;
+// 		clear_oled();
+// 	}
+// }
 
 void state_run_job(void){
 
-	char msg_temp[20]={0};
-	int _temp;
+	char msg_temp[17]={0};
+	int cnt_remain;
 	int enc_input, btn_click_input, btn_hold_input;
 	
+	// int shutter_val, period_val;
+
 	enc_input = get_enc_delta();
 	btn_click_input = get_status_btn_click();
 	btn_hold_input = get_status_btn_hold();
 	
-
-	_temp = state_var_num.var_set[state_var_num.idx];
-	_temp = state_var_shutter.var_set[state_var_shutter.idx];
-	_temp = state_var_period.var_set[state_var_period.idx];
+	// _temp = state_var_num.var_set[state_var_num.idx];
+	// shudtter_val = state_var_shutter.var_set[state_var_shutter.idx];
+	// period_val = state_var_period.var_set[state_var_period.idx];
 
 	// _temp = pulse_gen_100ms_unit(010, 30, 3, NULL);
 	// _temp = pulse_gen_w_preamble_100ms_unit(3,15,40,15,NULL);
 	
+	cnt_remain = pulse_gen_w_preamble_100ms_unit(
+		state_var_num.var_set[state_var_num.idx],
+		MAX(state_var_shutter.var_set[state_var_shutter.idx]*10, MIN_SHUTTER_100MS),
+		MAX(state_var_period.var_set[state_var_period.idx]*10, MIN_PERIOD_100MS),
+		MIN_PREAMBLE_100MS, NULL );
+
 	// _temp = pulse_gen_w_preamble_100ms_unit(
 	// 	state_var_num.var_set[state_var_num.idx],
-	// 	MAX(state_var_shutter.var_set[state_var_shutter.idx], 1),
-	// 	MAX(state_var_period.var_set[state_var_period.idx], 5),
+	// 	(state_var_shutter.var_set[state_var_shutter.idx]),
+	// 	(state_var_period.var_set[state_var_period.idx]),
 	// 	3, NULL );
+		
+	sprintf(msg_temp, "T_p:%4ds", state_var_period.var_set[state_var_period.idx]);
+	print_text(0, 0,msg_temp, NULL);
 
-	_temp = pulse_gen_w_preamble_100ms_unit(
-		state_var_num.var_set[state_var_num.idx],
-		(state_var_shutter.var_set[state_var_shutter.idx]),
-		(state_var_period.var_set[state_var_period.idx]),
-		3, NULL );
+	// sprintf(msg_temp, "Ts:%3ds Tp:%4ds", state_var_shutter.var_set[state_var_shutter.idx], state_var_period.var_set[state_var_period.idx]);
+	// print_text(0, 0,msg_temp, NULL);
 
-	sprintf(msg_temp, "value: %d", sizeof(state_var_t));
-	print_text(0, 3, msg_temp, 0);
+	sprintf(msg_temp, "Counts:%4d/%4d", state_var_num.var_set[state_var_num.idx]-cnt_remain+1, state_var_num.var_set[state_var_num.idx]);
+	print_text(0, 2, msg_temp, NULL);
 	
-	if (btn_hold_input) {
+	if (btn_hold_input || (cnt_remain == 0)) {
 		state_cur = STATE_MAIN;
+		pulse_gen_w_preamble_100ms_unit(0,0,0,0, PULSE_GEN_CMD_RESET );
 		clear_oled();
 	}
 }
@@ -303,11 +312,11 @@ void state_main_job(void){
 			}
 			break;
 
-		case STATE_TIME_LAG:
-			sprintf(msg_temp1, "<   Lag Time   >");
+		// case STATE_TIME_LAG:
+		// 	sprintf(msg_temp1, "<   Lag Time   >");
 			
-			sprintf(msg_temp2, state_var_lagtime.msg_format, state_var_lagtime.var_set[state_var_lagtime.idx]);
-			break;
+		// 	sprintf(msg_temp2, state_var_lagtime.msg_format, state_var_lagtime.var_set[state_var_lagtime.idx]);
+		// 	break;
 
 		case STATE_NUM_SHOT:
 			sprintf(msg_temp1, "< Num of Shots >");
@@ -336,12 +345,12 @@ void state_main_job(void){
 		if (state_var_period.var_set[state_var_period.idx] > state_var_shutter.var_set[state_var_shutter.idx]){
 			state_cur = STATE_RUN;
 			// pulse_gen_100ms_unit(10, 30, 3, PULSE_GEN_CMD_RUN);
-			pulse_gen_w_preamble_100ms_unit(3, 15,40,15,PULSE_GEN_CMD_RUN);
-			// pulse_gen_w_preamble_100ms_unit(
-			// 	state_var_num.var_set[state_var_num.idx],
-			// 	MAX(state_var_shutter.var_set[state_var_shutter.idx], 1),
-			// 	MAX(state_var_period.var_set[state_var_period.idx], 5),
-			// 	3, PULSE_GEN_CMD_RUN );
+			// pulse_gen_w_preamble_100ms_unit(3, 15,40,15,PULSE_GEN_CMD_RUN);
+			pulse_gen_w_preamble_100ms_unit(
+				state_var_num.var_set[state_var_num.idx],
+				MAX(state_var_shutter.var_set[state_var_shutter.idx]*10, MIN_SHUTTER_100MS),
+				MAX(state_var_period.var_set[state_var_period.idx]*10, MIN_PERIOD_100MS),
+				MIN_PREAMBLE_100MS, PULSE_GEN_CMD_RUN );
 
 			clear_oled();
 
@@ -406,9 +415,9 @@ int main(void)
 				state_shutter_job();
 				break;
 			
-			case STATE_TIME_LAG:
-				state_timelag_job();
-				break;
+			// case STATE_TIME_LAG:
+			// 	state_timelag_job();
+			// 	break;
 			
 			case STATE_RUN:
 				state_run_job();
